392e854345d475d8d7eba31ab21a9250f4da2d66
Vitor Gabriel Agner <144962857+Tatzumikk@users.noreply.github.com>
2025-11-14 19:41:32 -0300

    Add Javadoc comments to RelatorioStrategy interface

--- Prova/src/Q1/RelatorioStrategy.java
+++ Prova/src/Q1/RelatorioStrategy.java
@@ -1,5 +1,10 @@
 package Q1;
 
+/**
+ * Interface (Strategy) que define a operação padrão 
+ * para todas as formas de geração de relatório.
+ */
 public interface RelatorioStrategy {
     void gerar();
+
 }
